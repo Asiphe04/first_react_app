6abfbda146ce435a91592ff3d59b7d1e07cc1978
Asiphe04 <asiphendimlana40@gmail.com>
2023-11-07 09:24:46 +0200

    added a button

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@
 import {useState} from 'react';
 import './App.css';
 
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
 function App() {
   const [counter , setCounter] = useState(0);
   return (
     <div className="App">
       <button onClick={() => setCounter((prevCount) => prevCount - 1) }>-</button>
       <h1>{counter}</h1>
+      
       <button onClick={() => setCounter((prevCount) => prevCount + 1) }>+</button>
- 
+     <br/>
+      <MyButton />
 
 
     </div>
