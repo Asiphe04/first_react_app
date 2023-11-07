c009d8d7c931813be996eedcab99034a44002abe
Asiphe04 <asiphendimlana40@gmail.com>
2023-11-07 09:20:22 +0200

    increment working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+
 import {useState} from 'react';
 import './App.css';
 
@@ -6,23 +6,10 @@ function App() {
   const [counter , setCounter] = useState(0);
   return (
     <div className="App">
-      <button onclick={() => setCounter((prevCount) => prevCount - 1) }>-</button>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1) }>-</button>
       <h1>{counter}</h1>
-      <button onclick={() => setCounter((prevCount) => prevCount + 1) }>+</button>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <button onClick={() => setCounter((prevCount) => prevCount + 1) }>+</button>
+ 
 
 
     </div>
